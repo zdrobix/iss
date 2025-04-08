b30872549896d60fa97098826ee93616eef0ebb0
zdrobix <zdroba88@gmail.com>
2025-04-08 21:48:24 +0300

    Added User class instead of HospitalStaff and PharmacyStaff.

---
+++ api/PharmacyApi/PharmacyApi/Models/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿namespace PharmacyApi.Models.Domain
+{
+	public class User : Entity<int> 
+	{
+		public string Name { get; set; }
+		public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+		public User (string name, string username, string password, string role)
+		{
+			Name = name;
+			Username = username;
+			Password = password;
+			Role = role;
+		}
+	}
+}
