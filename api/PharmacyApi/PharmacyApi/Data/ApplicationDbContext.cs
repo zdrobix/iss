51a7a95c378221739ac998e8262d4b0a8da7d769
zdrobix <zdroba88@gmail.com>
2025-04-08 17:12:53 +0300

    Created DbContext class.

---
+++ api/PharmacyApi/PharmacyApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyApi.Models.Domain;
+
+namespace PharmacyApi.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+		public DbSet<Drug> Drugs { get; set; }
+		public DbSet<OrderedDrug> OrderedDrugs { get; set; }
+		public DbSet<StoredDrug> StoredDrugs { get; set; }
+		public DbSet<PlacedOrder> PlacedOrders { get; set; }
+		public DbSet<ResolvedOrder> ResolvedOrders { get; set; }
+		public DbSet<Pharmacy> Pharmacies { get; set; }
+		public DbSet<Hospital> Hospitals { get; set; }
+		public DbSet<HospitalStaff> HospitalStaff { get; set; }
+		public DbSet<PharmacyStaff> PharmacyStaff { get; set; }
+		public DbSet<DrugStorage> DrugStorages { get; set; }
+	}
+}
