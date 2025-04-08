d9dec81a6fce5e6db3ec4b79540fbba1fe0c402c
zdrobix <zdroba88@gmail.com>
2025-04-08 21:52:10 +0300

    Injected repositories into builder, also added password key.

--- api/PharmacyApi/PharmacyApi/Program.cs
+++ api/PharmacyApi/PharmacyApi/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using PharmacyApi.Data;
+using PharmacyApi.Repo.Implementation;
+using PharmacyApi.Repo.Interface;
+using PharmacyApi.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
 
+PasswordHasher.SetPasswordKey(builder.Configuration["Keys:Password"]!);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -13,6 +17,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddScoped<IDrugRepository, DrugRepository>();
+builder.Services.AddScoped<IDrugStorageRepository, DrugStorageRepository>();
+builder.Services.AddScoped<IHospitalRepository, HospitalRepository>();
+builder.Services.AddScoped<IPharmacyRepository, PharmacyRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -22,6 +32,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(options => {
+	options.AllowAnyHeader();
+	options.AllowAnyOrigin();
+	options.AllowAnyMethod();
+});
 
 app.UseAuthorization();
 
