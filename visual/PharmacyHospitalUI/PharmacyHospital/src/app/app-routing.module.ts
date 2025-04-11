131cbe7163c24bde0f9ea00b1acdc4d584dd0dbd
zdrobix <zdroba88@gmail.com>
2025-04-11 13:25:28 +0300

    Added empty angular project.

---
+++ visual/PharmacyHospitalUI/PharmacyHospital/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AccountComponent} from './features/account/account.component';
+
+const routes: Routes = [
+  {
+    path: '/account',
+    component: AccountComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
