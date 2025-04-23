e1ade53d6c3ef7fd700144aa8b8ea997fadc3ffe
zdrobix <zdroba88@gmail.com>
2025-04-24 00:05:40 +0300

    Added logging + implemented update method.

--- api/PharmacyApi/PharmacyApi/Controllers/PharmacyController.cs
+++ api/PharmacyApi/PharmacyApi/Controllers/PharmacyController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using PharmacyApi.Models.Domain;
 using PharmacyApi.Models.DTO;
 using PharmacyApi.Repo.Interface;
+using Serilog;
 
 namespace PharmacyApi.Controllers
 {
@@ -15,16 +16,17 @@ namespace PharmacyApi.Controllers
 		public PharmacyController(IPharmacyRepository pharmacyRepository)
 		{
 			this.pharmacyRepository = pharmacyRepository;
+			Log.Information("PharmacyController initialized");
 		}
 
 		// POST: https://localhost:7282/api/pharmacy
 		[HttpPost]
-		public async Task<IActionResult> CreatePharmacy([FromBody] PharmacyDTO pharmacyDTO)
+		public async Task<IActionResult> CreatePharmacy([FromBody] AddPharmacyRequestDTO request)
 		{
 			var pharmacy = new Pharmacy(
-				pharmacyDTO.Name, 
+				request.Name, 
 				new List<User>(),
-				new DrugStorage(),
+				new DrugStorage { StoredDrugs = new List<StoredDrug>() },
 				new List<PlacedOrder>(),
 				new List<ResolvedOrder>()
 			);
@@ -59,7 +61,7 @@ namespace PharmacyApi.Controllers
 			);
 		}
 
-		// GET: http://localhost:7282/api/pharmacies
+		// GET: http://localhost:7282/api/pharmacy
 		[HttpGet]
 		public async Task<IActionResult> GetAllPharmacies()
 		{
@@ -99,7 +101,7 @@ namespace PharmacyApi.Controllers
 			);
 		}
 
-		// GET: http://localhost:7282/api/pharmacies{id}
+		// GET: http://localhost:7282/api/pharmacy{id}
 		[HttpGet]
 		[Route("{id:int}")]
 		public async Task<IActionResult> GetPharmacyById(int id)
@@ -136,6 +138,86 @@ namespace PharmacyApi.Controllers
 
 				}
 			);
+		}
+
+		// PUT: http://localhost:7282/api/pharmacy{id}
+		[HttpPut]
+		[Route("{id:int}")]
+		public async Task<IActionResult> UpdatePharmacy([FromRoute] int id, [FromBody] UpdatePharmacyRequestDTO request)
+		{
+			Log.Information($"Searching for pharmacy with id {id}");
+
+			var existingPharmacy = await this.pharmacyRepository.GetById(id);
+			if (existingPharmacy == null)
+			{
+				Log.Information($"Couldn't find pharmacy with id {id}");
+				return NotFound();
+			}
+
+			existingPharmacy.Name = request.Name;
+
+			if (request.Staff != null)
+			{
+				existingPharmacy.Staff = request.Staff.Select(s => new User
+				{
+					Name = s.Name,
+					Username = s.Username,
+					Password = s.Password,
+					Role = s.Role
+				}).ToList();
+			} else Log.Information("No staff to update");
+
+			if (request.Storage != null && request.Storage.StoredDrugs != null)
+			{
+				existingPharmacy.Storage = new DrugStorage
+				{
+					StoredDrugs = request.Storage.StoredDrugs.Select(storedDrug => new StoredDrug
+					{
+						Quantity = storedDrug.Quantity,
+						Drug = new Drug
+						{
+							Name = storedDrug.Drug.Name,
+							Price = storedDrug.Drug.Price
+						}
+					}).ToList()
+				};
+			} else Log.Information("No storage to update");
+
+			Log.Information("Updating pharmacy with id {id}", id);
+			var updatedPharmacy = await pharmacyRepository.UpdateAsync(existingPharmacy);
+
+			if (updatedPharmacy == null)
+			{
+				Log.Information($"Couldn't update pharmacy with id {id}");
+				return NotFound();
+			}
+			return Ok(new PharmacyDTO
+					{
+					Id = updatedPharmacy.Id,
+					Name = updatedPharmacy.Name,
+					Staff = updatedPharmacy.Staff.Select(s => new UserDTO
+					{
+						Name = s.Name,
+						Username = s.Username,
+						Password = s.Password,
+						Role = s.Role
+					}).ToList(),
+					Storage = new DrugStorageDTO
+					{
+						StoredDrugs = updatedPharmacy.Storage.StoredDrugs
+									.Select(
+										storedDrug => new StoredDrugDTO
+										{
+											Quantity = storedDrug.Quantity,
+											Drug = new DrugDTO
+											{
+												Name = storedDrug.Drug.Name,
+												Price = storedDrug.Drug.Price
+											}
+										}
+									).ToList()
+					}
+				});
 		}
 	}
 }
