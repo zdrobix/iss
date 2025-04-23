9c993c120169da9694ff93fcc23b1c88222a3f49
zdrobix <zdroba88@gmail.com>
2025-04-24 00:09:43 +0300

    Created add user component.

---
+++ visual/PharmacyHospitalUI/src/app/features/admin/users/add-user/add-user.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AddUserRequest } from 'src/app/features/models/add-user-request.model';
+import { UsersService } from '../services/users.service';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements OnDestroy{
+  model: AddUserRequest;
+  private addUserSubscription?: Subscription;
+
+  constructor(private router: Router, private userService: UsersService) {
+    this.model = {
+      name: '',
+      username: '',
+      password: '',
+      role: ''
+    };
+  }
+
+  onFormSubmit() {
+    if (!this.model.name || !this.model.username || !this.model.password || !this.model.role) 
+      return;
+    this.addUserSubscription = this.userService.addUser(this.model).subscribe({
+      next: (response) => {
+        this.router.navigateByUrl('/users');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.addUserSubscription?.unsubscribe();
+  }
+
+}
