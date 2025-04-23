5da5c3abfaee3ca2f3b64967d4b12d7806d17eee
zdrobix <zdroba88@gmail.com>
2025-04-24 01:43:22 +0300

    Deleted useless component.

--- visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/pharmacy-list/pharmacy-list.component.ts
+++ visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/pharmacy-list/pharmacy-list.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pharmacy } from 'src/app/features/models/pharmacy.model';
 import { PharmaciesService } from 'src/app/features/admin/pharmacies/services/pharmacies.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pharmacy-list',
   templateUrl: './pharmacy-list.component.html',
   styleUrls: ['./pharmacy-list.component.css']
 })
+
 export class PharmacyListComponent implements OnInit{
   pharmacies$?: Observable<Pharmacy[]>;
   
