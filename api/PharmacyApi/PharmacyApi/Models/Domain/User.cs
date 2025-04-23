a8262f46f883849ecbd106d532e3b54b20cea88a
zdrobix <zdroba88@gmail.com>
2025-04-24 01:39:32 +0300

    2 way relation.

--- api/PharmacyApi/PharmacyApi/Models/Domain/User.cs
+++ api/PharmacyApi/PharmacyApi/Models/Domain/User.cs
@@ -7,6 +7,11 @@
         public string Password { get; set; }
         public string Role { get; set; }
 
+		public int? PharmacyId { get; set; }
+		public Pharmacy? Pharmacy { get; set; }
+		public int? HospitalId { get; set; }
+		public Hospital? Hospital { get; set; }
+
 		public User (string name, string username, string password, string role)
 		{
 			Name = name;
