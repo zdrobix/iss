9e79997b18ce226466e43e1828b2ca7ba33a7282
zdrobix <zdroba88@gmail.com>
2025-04-24 00:16:25 +0300

    The admin tab is only visible to admin users.

--- visual/PharmacyHospitalUI/src/app/core/components/navbar/navbar.component.ts
+++ visual/PharmacyHospitalUI/src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginService } from 'src/app/features/account/services/login.service';
+import { UserDTO } from 'src/app/features/models/user-dto.model';
+import { User } from 'src/app/features/models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  user$: Observable<UserDTO | null>;
+
+  constructor(private loginService: LoginService ) {
+    this.user$ = this.loginService.getLoggedInUser();
+   }
+
+  isAdmin(user: UserDTO | null): boolean {
+    return user?.role === "ADMIN";
+  }
 
 }
