6098a87386387df7b39ede7b72af9f8792ae0fdf
zdrobix <zdroba88@gmail.com>
2025-04-24 00:03:47 +0300

    Created dto's for update requests.

---
+++ api/PharmacyApi/PharmacyApi/Models/DTO/UpdateDrugRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmacyApi.Models.DTO
+{
+	public class UpdateDrugRequestDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public float Price { get; set; }
+	}
+}
---
+++ api/PharmacyApi/PharmacyApi/Models/DTO/UpdatePharmacyRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmacyApi.Models.DTO
+{
+	public class UpdatePharmacyRequestDTO
+	{
+		public string Name { get; set; }
+		public ICollection<UserDTO> Staff { get; set; }
+		public DrugStorageDTO Storage { get; set; }
+	}
+}
