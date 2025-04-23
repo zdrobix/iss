f875c20b11f52bd776166e54af5a1476b4ca8bc9
zdrobix <zdroba88@gmail.com>
2025-04-24 01:41:48 +0300

    Modified user model to fit the backend structure.

--- visual/PharmacyHospitalUI/src/app/features/models/user.model.ts
+++ visual/PharmacyHospitalUI/src/app/features/models/user.model.ts
@@ -1,7 +1,14 @@
+import { Hospital } from "./hospital.model";
+import { Pharmacy } from "./pharmacy.model";
+
 export interface User {
     id: number,
     username: string,
     name: string,
     password: string,
     role: string
+    pharmacyId: number | null,
+    hospitalId: number | null,
+    pharmacy: Pharmacy | null,
+    hospital: Hospital | null,
 }
