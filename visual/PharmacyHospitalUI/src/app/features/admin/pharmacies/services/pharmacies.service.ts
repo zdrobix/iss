fcee2f2556d0b2a1612eec3555a2f17627fc8252
zdrobix <zdroba88@gmail.com>
2025-04-24 00:10:18 +0300

    Renamed some parameters, fixed the http url.

--- visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/services/pharmacies.service.ts
+++ visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/services/pharmacies.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Pharmacy } from '../../../models/pharmacy.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environment/environment';
 import { AddPharmacyRequest } from '../../../models/add-pharmacy-request.model';
+import { Pharmacy } from 'src/app/features/models/pharmacy.model';
+import { UpdatePharmacyRequest } from 'src/app/features/models/update-pharmacy-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,19 @@ import { AddPharmacyRequest } from '../../../models/add-pharmacy-request.model';
 export class PharmaciesService {
   constructor(private http: HttpClient) { }
 
-  addPharmacy(model: AddPharmacyRequest) : Observable<void> {
-    return this.http.put<void>(`${environment.apiBaseUrl}/api/pharmacy`, model);
+  addPharmacy(request: AddPharmacyRequest) : Observable<Pharmacy> {
+    return this.http.post<Pharmacy>(`${environment.apiBaseUrl}/api/pharmacy`, request);
   }
 
   getPharmacies(): Observable<Pharmacy[]> {
-    return this.http.get<Pharmacy[]>(`${environment.apiBaseUrl}/api/pharmacies`);
+    return this.http.get<Pharmacy[]>(`${environment.apiBaseUrl}/api/pharmacy`);
+  }
+
+  getPharmacy(id: number): Observable<Pharmacy> {
+    return this.http.get<Pharmacy>(`${environment.apiBaseUrl}/api/pharmacy/${id}`);
+  }
+
+  updatePharmacy(id: number, request: UpdatePharmacyRequest): Observable<Pharmacy> {
+    return this.http.put<Pharmacy>(`${environment.apiBaseUrl}/api/pharmacy/${id}`, request);
   }
 }
