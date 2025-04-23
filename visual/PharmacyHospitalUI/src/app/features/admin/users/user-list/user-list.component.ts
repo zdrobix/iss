39cf215cfff27eb9b5a097fb52c9da131478fee3
zdrobix <zdroba88@gmail.com>
2025-04-24 00:09:07 +0300

    Created user list component. Displays all users.

---
+++ visual/PharmacyHospitalUI/src/app/features/admin/users/user-list/user-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from 'src/app/features/models/user.model';
+import { UsersService } from '../services/users.service';
+import { Subscription } from 'rxjs/internal/Subscription';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit, OnDestroy {
+
+  users$?: Observable<User[]>;
+  private deleteUserSubscription?: Subscription;
+
+
+  constructor(private usersService: UsersService) {
+    
+  }
+  ngOnInit(): void {
+    this.users$ = this.usersService.getUsers();
+  }
+
+  deleteUser(id: number) {
+    this.deleteUserSubscription = this.usersService.deleteUser(id).subscribe(() => {
+      this.users$ = this.usersService.getUsers();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.deleteUserSubscription?.unsubscribe();
+  }
+}
