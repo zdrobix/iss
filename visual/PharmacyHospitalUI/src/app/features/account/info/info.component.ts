487a6bb70bb3274d146eb401f4562ec3e4a65808
zdrobix <zdroba88@gmail.com>
2025-04-24 00:15:47 +0300

    Added logout functionality, in the info component.

--- visual/PharmacyHospitalUI/src/app/features/account/info/info.component.ts
+++ visual/PharmacyHospitalUI/src/app/features/account/info/info.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { UserDTO } from '../../models/user-dto.model';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnInit, OnDestroy {
   user$: Observable<UserDTO | null> | undefined;
+  private logoutSubscription?: Subscription;
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router) {}
 
   ngOnInit() {
     this.user$ = this.loginService.user$;
+  }
+
+  logout(){
+    this.logoutSubscription = this.loginService.logout().pipe(take(1)).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  ngOnDestroy() {
+      this.logoutSubscription?.unsubscribe();
   }
 }
