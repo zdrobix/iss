2b91552f192b826dad0ce1cb95ee7641ee3068a9
zdrobix <zdroba88@gmail.com>
2025-04-24 00:07:23 +0300

    Fixed the bug where after logout you're redirected to an empty account info page.

--- visual/PharmacyHospitalUI/src/app/app-routing.module.ts
+++ visual/PharmacyHospitalUI/src/app/app-routing.module.ts
@@ -8,16 +8,25 @@ import { AddDrugComponent } from './features/admin/drugs/add-drug/add-drug.compo
 import { AuthGuard } from './core/guards/auth.guard';
 import { PharmacyListComponent } from './features/admin/pharmacies/pharmacy-list/pharmacy-list.component';
 import { AddPharmacyComponent } from './features/admin/pharmacies/add-pharmacy/add-pharmacy.component';
+import { UserListComponent } from './features/admin/users/user-list/user-list.component';
+import { AddUserComponent } from './features/admin/users/add-user/add-user.component';
+import { EditStorageComponent } from './features/admin/pharmacies/edit-storage/edit-storage.component';
+import { EditStaffComponent } from './features/admin/pharmacies/edit-staff/edit-staff.component';
 
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent, runGuardsAndResolvers: 'always'},
   { path: 'account', component: InfoComponent, canActivate: [AuthGuard]},
   { path: 'drugs', component: DrugListComponent, canActivate: [AuthGuard]},
   { path: 'drugs/add', component: AddDrugComponent, canActivate: [AuthGuard]},
   { path: 'pharmacies', component: PharmacyListComponent, canActivate: [AuthGuard]},
   { path: 'pharmacies/add', component: AddPharmacyComponent, canActivate: [AuthGuard]},
-  { path: '**', redirectTo: '' }
+  { path: 'pharmacies/edit/staff/:id', component: EditStaffComponent, canActivate: [AuthGuard]},
+  { path: 'pharmacies/edit/storage/:id', component: EditStorageComponent, canActivate: [AuthGuard]},
+  { path: 'users', component: UserListComponent, canActivate: [AuthGuard]},
+  { path: 'users/add', component: AddUserComponent, canActivate: [AuthGuard]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
