321998a15d238355f7810e935456c90bde37ddbd
zdrobix <zdroba88@gmail.com>
2025-04-24 00:09:25 +0300

    Added services for user operations.

---
+++ visual/PharmacyHospitalUI/src/app/features/admin/users/services/users.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AddUserRequest } from 'src/app/features/models/add-user-request.model';
+import { User } from 'src/app/features/models/user.model';
+import { environment } from 'src/environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  addUser (user: AddUserRequest): Observable<void> {
+      return this.http.post<void>(`${environment.apiBaseUrl}/api/user`, user);
+  }
+
+  deleteUser (id: number): Observable<void> {
+      return this.http.delete<void>(`${environment.apiBaseUrl}/api/user/${id}`);
+  }
+
+  getUsers () : Observable<User[]> {
+      return this.http.get<User[]>(`${environment.apiBaseUrl}/api/user`);
+  }
+}
