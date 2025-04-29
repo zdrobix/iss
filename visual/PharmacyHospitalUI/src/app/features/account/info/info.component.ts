ccccb03618dc59beb4c1d376a11ec5a4e2e03b79
zdrobix <zdroba88@gmail.com>
2025-04-29 16:35:19 +0300

    Should delete these methods.

--- visual/PharmacyHospitalUI/src/app/features/account/info/info.component.ts
+++ visual/PharmacyHospitalUI/src/app/features/account/info/info.component.ts
@@ -3,6 +3,8 @@ import { LoginService } from '../services/login.service';
 import { Observable, Subscription, take } from 'rxjs';
 import { Router } from '@angular/router';
 import { User } from '../../models/user.model';
+import { PharmaciesService } from '../../admin/pharmacies/services/pharmacies.service';
+import { HospitalsService } from '../../admin/hospitals/services/hospitals.service';
 
 @Component({
   selector: 'app-info',
@@ -12,11 +14,37 @@ import { User } from '../../models/user.model';
 export class InfoComponent implements OnInit, OnDestroy {
   user$: Observable<User | null> | undefined;
   private logoutSubscription?: Subscription;
+  private getPharmacySubscription?: Subscription;
+  private getHospitalSubscription?: Subscription;
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private loginService: LoginService, private pharmaciesService: PharmaciesService, private hospitalsService: HospitalsService, private router: Router) {}
 
   ngOnInit() {
     this.user$ = this.loginService.user$;
+  }
+
+  getPharmacy(id: number) {
+    if (!id)
+      return;
+    let pharmacyName = '';
+    this.getPharmacySubscription = this.pharmaciesService.getPharmacy(id).pipe(take(1)).subscribe({
+      next: (pharmacy) => {
+        pharmacyName = pharmacy.name;
+      }
+    });
+    return pharmacyName;
+  }
+
+  getHospital(id: number) {
+    if (!id)
+      return;
+    let hospitalName = '';
+    this.getHospitalSubscription = this.hospitalsService.getHospital(id).pipe(take(1)).subscribe({
+      next: (hospital) => {
+        hospitalName = hospital.name;
+      }
+    });
+    return hospitalName;
   }
 
   logout(){
@@ -29,5 +57,7 @@ export class InfoComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
       this.logoutSubscription?.unsubscribe();
+      this.getPharmacySubscription?.unsubscribe();
+      this.getHospitalSubscription?.unsubscribe();
   }
 }
