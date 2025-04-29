f291e718de35c76e627e1b8371248e9acf7a457c
zdrobix <zdroba88@gmail.com>
2025-04-29 16:33:32 +0300

    Created controller for orders.

---
+++ api/PharmacyApi/PharmacyApi/Controllers/OrderController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyApi.Models.Domain;
+using PharmacyApi.Models.DTO;
+using PharmacyApi.Repo.Interface;
+using Serilog;
+
+namespace PharmacyApi.Controllers
+{
+    [Route("api/[controller]")]
+	[ApiController]
+	public class OrderController : ControllerBase
+	{
+		private readonly IUserRepository userRepository;
+		private readonly IDrugRepository drugRepository;
+		private readonly IOrderRepository orderRepository;
+
+		public OrderController(IOrderRepository orderRepository, IUserRepository userRepository, IDrugRepository drugRepository)
+		{
+			this.orderRepository = orderRepository;
+			this.userRepository = userRepository;
+			this.drugRepository = drugRepository;
+		}
+
+		// POST : https://localhost:7282/api/order
+		[HttpPost]
+		public async Task<IActionResult> CreateOrder([FromBody] AddOrderRequestDTO request)
+		{
+			Log.Information("CreateOrder method called.");
+			if (request == null)
+			{
+				Log.Error("Received null request.");
+				return BadRequest();
+			}
+			Log.Information($"Creating order by {request.PlacedBy.Name}");
+			var order = new Order
+			{
+				PlacedBy = await userRepository.GetById(request.PlacedBy.Id),
+				OrderedDrugs = await Task.WhenAll(request.OrderedDrugs.Select(async d =>
+										{
+											var drug = await drugRepository.GetById(d.Drug.Id);
+
+											return new OrderedDrug
+											{
+												Drug = drug,
+												Quantity = d.Quantity
+											};
+										})),
+				DateTime = request.DateTime
+			};
+
+			Log.Information("Adding order.");
+			order = await this.orderRepository.CreateAsync(order);
+			return Ok(
+				DomainModelToDTO(order!)
+			);
+		}
+
+		// GET : https://localhost:7282/api/order
+		[HttpGet]
+		public async Task<IActionResult> GetAllOrders() =>
+			await this.orderRepository.GetAllAsync() is IEnumerable<Order> orders
+				? Ok(orders.Select(order =>
+					DomainModelToDTO(order!)
+				).ToList()) : NotFound();
+
+		// GET : https://localhost:7282/api/order
+		[HttpGet("unresolved")]
+		public async Task<IActionResult> GetAllUnresolvedOrders() =>
+			await this.orderRepository.GetAllAsync() is IEnumerable<Order> orders
+				? Ok(orders
+					.Where(o => o.ResolvedBy == null)
+					.Select(order =>
+					DomainModelToDTO(order!)
+				).ToList()) : NotFound();
+
+		// GET : https://localhost:7282/api/order{id}
+		[HttpGet]
+		[Route("{id:int}")]
+		public async Task<IActionResult> GetOrderById([FromRoute] int id)
+		{
+			var order = await this.orderRepository.GetById(id);
+			if (order == null)
+			{
+				Log.Information($"Couldn't find the order with id {id}");
+				return NotFound();
+			}
+			return Ok(
+				DomainModelToDTO(order!)
+			);
+		}
+
+		// POST : https://localhost:7383/api/order{id}
+		[HttpPost]
+		[Route("{id:int}")]
+		public async Task<IActionResult> UpdateOrder([FromRoute] int id, [FromBody] UpdateOrderRequestDTO request)
+		{
+			var order = await this.orderRepository.GetById(id);
+			if (order == null)
+			{
+				return NotFound();
+			}
+			if (request.ResolvedBy == null)
+			{
+				return BadRequest("User cannot be null");
+			}
+			order.ResolvedBy = (User) new User { 
+				Name = request.ResolvedBy.Name, 
+				Role = request.ResolvedBy.Role, 
+			}.SetId(id);
+			order = await this.orderRepository.UpdateAsync(order);
+			return Ok(
+				DomainModelToDTO(order!)	
+			);
+		}
+
+		public static OrderDTO DomainModelToDTO(Order order) =>
+			new OrderDTO
+			{
+				Id = order.Id,
+				PlacedBy = new UserDTO
+				{
+					Id = order.PlacedBy.Id,
+					Name = order.PlacedBy.Name,
+					Username = order.PlacedBy.Username,
+					Role = order.PlacedBy.Role,
+					Password = "*****",
+					Hospital = order.PlacedBy.Hospital == null ? null : new HospitalDTO 
+					{ 
+						Id = order.PlacedBy.Hospital.Id,
+						Name = order.PlacedBy.Hospital.Name
+					},
+					Pharmacy = null
+				},
+				ResolvedBy =
+					order.ResolvedBy == null ?
+					null :
+					new UserDTO
+					{
+						Id = order.ResolvedBy.Id,
+						Name = order.ResolvedBy.Name,
+						Username = order.ResolvedBy.Username,
+						Role = order.ResolvedBy.Role,
+						Password = "*****",
+						Hospital = null,
+						Pharmacy = order.ResolvedBy.Pharmacy == null ? null : new PharmacyDTO
+						{
+							Id = order.ResolvedBy.Pharmacy.Id,
+							Name = order.ResolvedBy.Pharmacy.Name,
+							Storage = new DrugStorageDTO()
+						}
+					},
+				OrderedDrugs = order.OrderedDrugs.Select(
+							d => new OrderedDrugDTO
+							{
+								Drug = new DrugDTO
+								{
+									Id = d.Drug.Id,
+									Name = d.Drug.Name,
+									Price = d.Drug.Price,
+								},
+								Quantity = d.Quantity
+							}
+				).ToList(),
+				DateTime = order.DateTime
+			};
+	}
+}
