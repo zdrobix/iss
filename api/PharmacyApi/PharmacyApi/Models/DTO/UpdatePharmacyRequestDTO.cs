d76724504cd3c5187403303ebc6f47525b17f377
zdrobix <zdroba88@gmail.com>
2025-04-29 16:29:29 +0300

    Removed staff list.

--- api/PharmacyApi/PharmacyApi/Models/DTO/UpdatePharmacyRequestDTO.cs
+++ api/PharmacyApi/PharmacyApi/Models/DTO/UpdatePharmacyRequestDTO.cs
@@ -3,7 +3,6 @@
 	public class UpdatePharmacyRequestDTO
 	{
 		public string Name { get; set; }
-		public ICollection<UserDTO> Staff { get; set; }
 		public DrugStorageDTO Storage { get; set; }
 	}
 }
