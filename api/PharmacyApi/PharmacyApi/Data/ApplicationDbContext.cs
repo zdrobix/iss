a712e091f9d085e8d1eb0150a8349d37ec1776d6
zdrobix <zdroba88@gmail.com>
2025-04-29 16:31:54 +0300

    Removed sets for orders, added set for OrderContainers.

--- api/PharmacyApi/PharmacyApi/Data/ApplicationDbContext.cs
+++ api/PharmacyApi/PharmacyApi/Data/ApplicationDbContext.cs
@@ -5,18 +5,23 @@ namespace PharmacyApi.Data
 {
 	public class ApplicationDbContext : DbContext
 	{
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
+		public ApplicationDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
 
 		public DbSet<Drug> Drugs { get; set; }
 		public DbSet<OrderedDrug> OrderedDrugs { get; set; }
 		public DbSet<StoredDrug> StoredDrugs { get; set; }
-		public DbSet<PlacedOrder> PlacedOrders { get; set; }
-		public DbSet<ResolvedOrder> ResolvedOrders { get; set; }
+		public DbSet<Order> Orders { get; set; }
 		public DbSet<Pharmacy> Pharmacies { get; set; }
 		public DbSet<Hospital> Hospitals { get; set; }
 		public DbSet<User> Users { get; set; }
 		public DbSet<DrugStorage> DrugStorages { get; set; }
+		public DbSet<OrderEntityContainer> OrderContainers { get; set; }
 	}
 }
