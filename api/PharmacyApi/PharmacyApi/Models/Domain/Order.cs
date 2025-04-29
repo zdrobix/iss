06898c9ed4cc2c634c667ea82ca5d90fa45207a1
zdrobix <zdroba88@gmail.com>
2025-04-29 16:27:00 +0300

    Made Order main class.

--- api/PharmacyApi/PharmacyApi/Models/Domain/Order.cs
+++ api/PharmacyApi/PharmacyApi/Models/Domain/Order.cs
@@ -2,17 +2,22 @@
 {
 	public class Order : Entity<int>
 	{
+		public User PlacedBy { get; set; }
+		public User? ResolvedBy { get; set; }
         public ICollection<OrderedDrug> OrderedDrugs { get; set; }
         public DateTime DateTime{ get; set; }
+	
 
-        public Order(ICollection<OrderedDrug> orderedDrugs, DateTime dateTime)
+		public Order(User placedBy, ICollection<OrderedDrug> orderedDrugs, DateTime dateTime)
 		{
+			PlacedBy = placedBy;
 			OrderedDrugs = orderedDrugs;
 			DateTime = dateTime;
 		}
 
 		public Order()
 		{
+			PlacedBy = new User();
 			OrderedDrugs = new List<OrderedDrug>();
 		}
 	}
