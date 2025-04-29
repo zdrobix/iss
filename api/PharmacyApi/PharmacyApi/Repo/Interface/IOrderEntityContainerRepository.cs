b27daeff5ed2b7afed959f4d4bbbc20ab74e4681
zdrobix <zdroba88@gmail.com>
2025-04-29 16:30:39 +0300

    Created OrderRepository interface and implementation.

---
+++ api/PharmacyApi/PharmacyApi/Repo/Interface/IOrderEntityContainerRepository.cs
@@ -0,0 +1,13 @@
+﻿using PharmacyApi.Models.Domain;
+
+namespace PharmacyApi.Repo.Interface
+{
+	public interface IOrderEntityContainerRepository
+	{
+		Task<OrderEntityContainer> CreateAsync(OrderEntityContainer orderContainer);
+		Task<IEnumerable<OrderEntityContainer>> GetAllAsync();
+		Task<OrderEntityContainer?> GetById(int id);
+		Task<OrderEntityContainer?> UpdateAsync(OrderEntityContainer orderContainer);
+		Task<OrderEntityContainer> DeleteAsync(int id);
+	}
+}
---
+++ api/PharmacyApi/PharmacyApi/Repo/Interface/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using PharmacyApi.Models.Domain;
+
+namespace PharmacyApi.Repo.Interface
+{
+	public interface IOrderRepository
+	{
+		Task<Order> CreateAsync(Order order);
+		Task<IEnumerable<Order>> GetAllAsync();
+		Task<Order?> GetById(int id);
+		Task<Order?> UpdateAsync(Order order);
+		Task<Order> DeleteAsync(int id);
+	}
+}
