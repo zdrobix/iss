1eafb4efe2a7f7dd9e7566b52833e8c7ee6888a3
zdrobix <zdroba88@gmail.com>
2025-04-29 16:28:55 +0300

    Added relevant information.

--- api/PharmacyApi/PharmacyApi/Models/DTO/OrderDTO.cs
+++ api/PharmacyApi/PharmacyApi/Models/DTO/OrderDTO.cs
@@ -2,6 +2,9 @@
 {
 	public class OrderDTO
 	{
+		public int Id { get; set; }
+		public UserDTO PlacedBy { get; set; }
+		public UserDTO? ResolvedBy {  get; set; }
         public ICollection<OrderedDrugDTO> OrderedDrugs { get; set; }
         public DateTime DateTime{ get; set; }
 	}
