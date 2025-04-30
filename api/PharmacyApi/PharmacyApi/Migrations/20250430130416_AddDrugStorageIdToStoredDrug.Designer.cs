34bd4c6337e3881cbf0dda1190e91b7a2605e501
zdrobix <zdroba88@gmail.com>
2025-05-01 02:21:06 +0300

    Database migration.

--- api/PharmacyApi/PharmacyApi/Migrations/20250430130416_AddDrugStorageIdToStoredDrug.Designer.cs
+++ api/PharmacyApi/PharmacyApi/Migrations/20250430130416_AddDrugStorageIdToStoredDrug.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PharmacyApi.Data;
 
@@ -11,9 +12,11 @@ using PharmacyApi.Data;
 namespace PharmacyApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250430130416_AddDrugStorageIdToStoredDrug")]
+    partial class AddDrugStorageIdToStoredDrug
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/PharmacyApi/PharmacyApi/Migrations/20250430130416_AddDrugStorageIdToStoredDrug.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PharmacyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDrugStorageIdToStoredDrug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoredDrugs_DrugStorages_DrugStorageId",
+                table: "StoredDrugs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DrugStorageId",
+                table: "StoredDrugs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoredDrugs_DrugStorages_DrugStorageId",
+                table: "StoredDrugs",
+                column: "DrugStorageId",
+                principalTable: "DrugStorages",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StoredDrugs_DrugStorages_DrugStorageId",
+                table: "StoredDrugs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DrugStorageId",
+                table: "StoredDrugs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StoredDrugs_DrugStorages_DrugStorageId",
+                table: "StoredDrugs",
+                column: "DrugStorageId",
+                principalTable: "DrugStorages",
+                principalColumn: "Id");
+        }
+    }
+}
