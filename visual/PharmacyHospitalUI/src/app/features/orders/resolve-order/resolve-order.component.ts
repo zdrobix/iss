3290f20adf69078398f9d10626f20ebf7301cafc
zdrobix <zdroba88@gmail.com>
2025-05-01 02:26:54 +0300

    Resolve order component. Problems with canResolve()

--- visual/PharmacyHospitalUI/src/app/features/orders/resolve-order/resolve-order.component.ts
+++ visual/PharmacyHospitalUI/src/app/features/orders/resolve-order/resolve-order.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, Subscription, take, tap } from 'rxjs';
+import { OrdersService } from '../services/orders.service';
+import { Order } from '../../models/order.model';
+import { OrderedDrug } from '../../models/ordered-drug.model';
+import { DrugStorage } from '../../models/drug-storage.model';
+import { User } from '../../models/user.model';
+import { LoginService } from '../../account/services/login.service';
+import { StoredDrug } from '../../models/stored-drug.mode';
 
 @Component({
   selector: 'app-resolve-order',
@@ -6,14 +14,68 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   styleUrls: ['./resolve-order.component.css']
 })
 export class ResolveOrderComponent implements OnInit, OnDestroy{
+  storageId?: number;
+  orders$?: Observable<Order[]>;
+  selectedOrder$?: Observable<Order>;
+  drugStorage$?: Observable<DrugStorage>;
+  isAvailable: boolean | null = null;
+  private getOrderedDrugsSubscription?: Subscription;
+  private getLoggedInUserSubscription?: Subscription;
+  private getStoredDrugSubscription?: Subscription;
 
-  constructor () {}
+  constructor (private ordersService: OrdersService, private loginsService: LoginService) {}
+
+  selectOrder(order: Order) {
+    this.selectedOrder$= this.ordersService.getOrder(order.id);
+  }
+
+  resolveOrder() {
+    if (!this.canBeResolved()) {
+      console.log("Order cannot be resolved");
+      return;
+    }
+    console.log("Resolving order");
+  }
+
+
+  ngOnInit(): void {
+    this.orders$ = this.ordersService.getUnresolvedOrders();
+    this.getLoggedInUserSubscription = this.loginsService.getLoggedInUser().pipe(take(1)).subscribe((user: User | null) => {
+      if (user) {
+        if (user.pharmacy) {
+          this.storageId = user.pharmacy.storage.id;
+        }
+      }});
+      if (this.storageId)
+        this.drugStorage$ = this.ordersService.getDrugStorage(this.storageId);
+  }
+
+  canBeResolved() : boolean {
+    if (!this.selectedOrder$) {
+      return false
+    }
+    if (!this.storageId) {
+      return false;
+    }
+    let valid = true;
+    this.getOrderedDrugsSubscription = this.selectedOrder$.pipe(take(1)).subscribe((order: Order) => {
+      order.orderedDrugs.forEach((orderedDrug: OrderedDrug) => { 
+        this.getStoredDrugSubscription = this.ordersService.getStoredDrug(orderedDrug.drug.id, this.storageId!).pipe(take(1)).subscribe((storedDrug: StoredDrug) => {
+          if (storedDrug) {
+            console.log("Stored drug: ", storedDrug);
+            if (storedDrug.quantity < orderedDrug.quantity) {
+              valid = false;
+            }
+          } 
+        })
+      });
+  });
+    return valid;
+  }
 
   ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
+    this.getOrderedDrugsSubscription?.unsubscribe();
+    this.getLoggedInUserSubscription?.unsubscribe();
+    this.getStoredDrugSubscription?.unsubscribe();
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
 }
