57cc1b51224fdfd032c22ddd5288dd374b19b12b
zdrobix <zdroba88@gmail.com>
2025-04-30 16:00:01 +0300

    Created Update request for drugStorage.

---
+++ api/PharmacyApi/PharmacyApi/Models/DTO/UpdateDrugStorageRequestDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace PharmacyApi.Models.DTO
+{
+	public class UpdateDrugStorageRequestDTO
+	{
+		public ICollection<StoredDrugDTO> StoredDrugs { get; set; } = new List<StoredDrugDTO>();
+	}
+}
