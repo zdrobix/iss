67fdef3773d78cf32b4083d1d09e3da6bec9b20a
zdrobix <zdroba88@gmail.com>
2025-05-01 02:23:05 +0300

    Add stored drug method.

--- visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/services/pharmacies.service.ts
+++ visual/PharmacyHospitalUI/src/app/features/admin/pharmacies/services/pharmacies.service.ts
@@ -4,7 +4,10 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environment/environment';
 import { AddPharmacyRequest } from '../../../models/add-pharmacy-request.model';
 import { Pharmacy } from 'src/app/features/models/pharmacy.model';
-import { UpdatePharmacyRequest } from 'src/app/features/models/update-pharmacy-request.model';
+import { UpdateDrugStorageRequest } from 'src/app/features/models/update-drogstorage-request.model';
+import { DrugStorage } from 'src/app/features/models/drug-storage.model';
+import { StoredDrug } from 'src/app/features/models/stored-drug.mode';
+import { AddStoredDrugRequest } from 'src/app/features/models/add-storeddrug-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +27,12 @@ export class PharmaciesService {
     return this.http.get<Pharmacy>(`${environment.apiBaseUrl}/api/pharmacy/${id}`);
   }
 
-  updatePharmacy(id: number, request: UpdatePharmacyRequest): Observable<Pharmacy> {
-    return this.http.put<Pharmacy>(`${environment.apiBaseUrl}/api/pharmacy/${id}`, request);
+  updateDrugStorage(id: number, request: UpdateDrugStorageRequest): Observable<DrugStorage> {
+    return this.http.put<DrugStorage>(`${environment.apiBaseUrl}/api/drugstorage/${id}`, request);
+  }
+
+  addUpdateStoredDrug(request: AddStoredDrugRequest): Observable<StoredDrug> {
+    console.log("Request: ", request);
+    return this.http.post<StoredDrug>(`${environment.apiBaseUrl}/api/storeddrug`, request);
   }
 }
