4461be53320ee47d43f71732d648c21b4b8b2bf9
zdrobix <zdroba88@gmail.com>
2025-04-30 16:02:57 +0300

    Major changes in structure.

--- api/PharmacyApi/PharmacyApi/Controllers/DrugStorageController.cs
+++ api/PharmacyApi/PharmacyApi/Controllers/DrugStorageController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PharmacyApi.Models.Domain;
 using PharmacyApi.Models.DTO;
+using PharmacyApi.Repo.Implementation;
 using PharmacyApi.Repo.Interface;
 
 namespace PharmacyApi.Controllers
@@ -11,10 +12,14 @@ namespace PharmacyApi.Controllers
 	public class DrugStorageController : ControllerBase
 	{
 		private readonly IDrugStorageRepository drugStorageRepository;
+		private readonly IDrugRepository drugRepository;
+		private readonly IStoredDrugRepository storedDrugRepository;
 
-        public DrugStorageController(IDrugStorageRepository drugStorageRepository)
-        {
+		public DrugStorageController(IDrugStorageRepository drugStorageRepository, IDrugRepository drugRepository, IStoredDrugRepository storedDrugRepository)
+		{
 			this.drugStorageRepository = drugStorageRepository;
+			this.drugRepository = drugRepository;
+			this.storedDrugRepository = storedDrugRepository;
 		}
 
 		// GET: http://localhost:7282/api/drugstorage{id}
@@ -31,16 +36,16 @@ namespace PharmacyApi.Controllers
 					StoredDrugs = drugStorage.StoredDrugs
 						.Select(
 							storedDrug => new StoredDrugDTO
+							{
+								Quantity = storedDrug.Quantity,
+								Drug = new DrugDTO
 								{
-									Quantity = storedDrug.Quantity,
-									Drug = new DrugDTO
-										{
-											Name = storedDrug.Drug.Name,
-											Price = storedDrug.Drug.Price
-										}
+									Name = storedDrug.Drug.Name,
+									Price = storedDrug.Drug.Price
 								}
+							}
 						).ToList()
-				}	
+				}
 			);
 		}
 
@@ -72,5 +77,55 @@ namespace PharmacyApi.Controllers
 			);
 		}
 
+		// PUT : http://localhost:7282/api/drugstorage{id}
+		[HttpPut]
+		[Route("{id:int}")]
+		public async Task<IActionResult> UpdateDrugStorage([FromRoute] int id, [FromBody] UpdateDrugStorageRequestDTO request, IDrugStorageRepository drugStorageRepository)
+		{
+			var drugStorage = await drugStorageRepository.GetById(id);
+			if (drugStorage == null)
+			{
+				return NotFound();
+			}
+
+			foreach (var storedDrug in request.StoredDrugs)
+			{
+				var existing = await this.storedDrugRepository.GetByDrugIdAndStorageId(storedDrug.Drug.Id, id);
+				if (existing == null)
+				{
+					var newStoredDrug = new StoredDrug
+					{
+						Drug = await this.drugRepository.GetById(storedDrug.Drug.Id),
+						Quantity = storedDrug.Quantity,
+						Storage = await this.drugStorageRepository.GetById(id)
+					};
+
+					await this.storedDrugRepository.CreateAsync(newStoredDrug);
+				} else
+				{
+					existing.Quantity = storedDrug.Quantity;
+					await this.storedDrugRepository.UpdateAsync(existing);
+				}
+			}
+
+			drugStorage = await drugStorageRepository.GetById(id);
+
+			return drugStorage == null ? BadRequest() : Ok(
+			new DrugStorageDTO
+			{
+				StoredDrugs = drugStorage.StoredDrugs
+					.Select(
+						storedDrug => new StoredDrugDTO
+						{
+							Quantity = storedDrug.Quantity,
+							Drug = new DrugDTO
+							{
+								Name = storedDrug.Drug.Name,
+								Price = storedDrug.Drug.Price
+							}
+						}
+					).ToList()
+			});
+		}
 	}
 }
