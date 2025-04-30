adb63038856093ea335458527ef5a12586988273
zdrobix <zdroba88@gmail.com>
2025-04-30 16:01:44 +0300

    Added DrugStorage.

--- api/PharmacyApi/PharmacyApi/Models/Domain/StoredDrug.cs
+++ api/PharmacyApi/PharmacyApi/Models/Domain/StoredDrug.cs
@@ -6,15 +6,20 @@ namespace PharmacyApi.Models.Domain
 	{
 		public int Quantity { get; set; }
 		public Drug Drug { get; set; }
+		public int DrugStorageId { get; set; }
+		public DrugStorage Storage { get; set; }
 
-		public StoredDrug (int quantity, Drug drug)
+
+		public StoredDrug (int quantity, Drug drug, DrugStorage storage)
 		{
 			Quantity = quantity;
 			Drug = drug;
+			Storage = storage;
 		}
 
 		public StoredDrug() {
 			Drug = new Drug();
+			Storage = new DrugStorage();
 		}
 	}
 }
