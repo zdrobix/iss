4d53bd350de220655458c07d60cea40989844e72
zdrobix <zdroba88@gmail.com>
2025-05-05 21:55:01 +0300

    Resolve order works correctly updating orders and stored drugs.

--- visual/PharmacyHospitalUI/src/app/features/orders/resolve-order/resolve-order.component.ts
+++ visual/PharmacyHospitalUI/src/app/features/orders/resolve-order/resolve-order.component.ts
@@ -8,6 +8,9 @@ import { User } from '../../models/user.model';
 import { LoginService } from '../../account/services/login.service';
 import { StoredDrug } from '../../models/stored-drug.mode';
 import { Drug } from '../../models/drug.model';
+import { UpdateOrderRequest } from '../../models/update-order-request.model';
+import { Router } from '@angular/router';
+import { AddStoredDrugRequest } from '../../models/add-storeddrug-request.model';
 
 @Component({
   selector: 'app-resolve-order',
@@ -16,35 +19,67 @@ import { Drug } from '../../models/drug.model';
 })
 export class ResolveOrderComponent implements OnInit, OnDestroy{
   storageId?: number;
+  loggedInUser?: User;
   orderIsValid?: boolean;
   orders$?: Observable<Order[]>;
+  selectedOrder?: Order;
   selectedOrder$?: Observable<Order>;
   drugStorage$?: Observable<DrugStorage>;
+  drugStorage?: DrugStorage;
   isAvailable: boolean | null = null;
   unavailableDrugs?: Drug[];
+  storedDrugs?: StoredDrug[];
   private getOrderedDrugsSubscription?: Subscription;
+  private getSelectedOrderSubscription?: Subscription;
   private getLoggedInUserSubscription?: Subscription;
   private getStoredDrugSubscription?: Subscription;
-  
+  private updateOrderSubscription?: Subscription;
+  private getDrugStorageSubscription?: Subscription;
 
-  constructor (private ordersService: OrdersService, private loginsService: LoginService) {}
+  constructor (private ordersService: OrdersService, private loginsService: LoginService, private router: Router) {}
 
   selectOrder(order: Order) {
     this.unavailableDrugs = [];
+    this.storedDrugs = [];
     this.selectedOrder$= this.ordersService.getOrder(order.id);
     this.checkOrder();
   }
 
   resolveOrder() {
-    if (!this.selectedOrder$) {
+    if (!this.selectedOrder$) 
       return;
-    }
-    this.selectedOrder$.pipe(take(1)).subscribe((order: Order) => { console.log(order)});
-    if (!this.orderIsValid) {
-      console.log("Order cannot be resolved");
+
+    this.getSelectedOrderSubscription = this.selectedOrder$.pipe(take(1)).subscribe((order: Order) => { 
+      this.selectedOrder = order;  
+    });
+    
+    if (!this.orderIsValid) 
       return;
-    }
-    console.log("Resolving order");
+
+    if (!this.selectedOrder || !this.loggedInUser) return;
+    this.selectedOrder?.orderedDrugs.forEach((orderedDrug: OrderedDrug) => {
+      if (!this.storedDrugs) return;
+      const updateStoredDrugRequest: AddStoredDrugRequest = {
+        quantity: this.storedDrugs?.find(storedDrug => storedDrug.drug.id === orderedDrug.drug.id)?.quantity! - orderedDrug.quantity,
+        drug: orderedDrug.drug,
+        storageId: this.drugStorage!.id
+      };
+      this.ordersService.updateStoredDrug(updateStoredDrugRequest).pipe(take(1)).subscribe((storedDrug: StoredDrug) => {
+        console.log(storedDrug);
+      });
+    });
+
+    const updateOrderRequest: UpdateOrderRequest = {
+      resolvedById: this.loggedInUser.id
+    };
+
+    this.updateOrderSubscription = this.ordersService.updateOrder(this.selectedOrder.id, updateOrderRequest).subscribe((order: Order) => {
+      this.orders$ = this.ordersService.getUnresolvedOrders(); 
+      this.selectedOrder = undefined;
+      this.unavailableDrugs = [];
+      this.storedDrugs = [];
+      this.router.navigateByUrl('/order/resolve');
+    });
   }
 
   isDrugAvailable(drug: Drug): boolean {
@@ -60,12 +95,18 @@ export class ResolveOrderComponent implements OnInit, OnDestroy{
     this.orders$ = this.ordersService.getUnresolvedOrders();
     this.getLoggedInUserSubscription = this.loginsService.getLoggedInUser().pipe(take(1)).subscribe((user: User | null) => {
       if (user) {
+        this.loggedInUser = user;
         if (user.pharmacy) {
           this.storageId = user.pharmacy.storage.id;
         }
       }});
-      if (this.storageId)
+      if (this.storageId) {
         this.drugStorage$ = this.ordersService.getDrugStorage(this.storageId);
+        this.getDrugStorageSubscription = this.drugStorage$.pipe(take(1)).subscribe((storage: DrugStorage) => {
+          this.drugStorage = storage;
+        });
+      }
+
   }
 
   checkOrder() : void {
@@ -79,10 +120,12 @@ export class ResolveOrderComponent implements OnInit, OnDestroy{
       return;
     }
     this.unavailableDrugs = [];
+    this.storedDrugs = [];
     this.getOrderedDrugsSubscription = this.selectedOrder$.pipe(take(1)).subscribe((order: Order) => {
       order.orderedDrugs.forEach((orderedDrug: OrderedDrug) => { 
         this.getStoredDrugSubscription = this.ordersService.getStoredDrug(orderedDrug.drug.id, this.storageId!).pipe(take(1)).subscribe((storedDrug: StoredDrug) => {
           if (storedDrug) {
+            this.storedDrugs?.push(storedDrug);
             if (storedDrug.quantity < orderedDrug.quantity) {
               this.orderIsValid = false;
               this.unavailableDrugs?.push(orderedDrug.drug);
@@ -97,5 +140,8 @@ export class ResolveOrderComponent implements OnInit, OnDestroy{
     this.getOrderedDrugsSubscription?.unsubscribe();
     this.getLoggedInUserSubscription?.unsubscribe();
     this.getStoredDrugSubscription?.unsubscribe();
+    this.updateOrderSubscription?.unsubscribe();
+    this.getSelectedOrderSubscription?.unsubscribe();
+    this.getDrugStorageSubscription?.unsubscribe();
   }
 }
