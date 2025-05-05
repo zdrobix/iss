5258cc50d1de6b0785e5c0bff37150ab9a4bda72
zdrobix <zdroba88@gmail.com>
2025-05-05 21:54:08 +0300

    Created empty component for history of orders.

---
+++ visual/PharmacyHospitalUI/src/app/features/orders/history-order/history-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-order',
+  templateUrl: './history-order.component.html',
+  styleUrls: ['./history-order.component.css']
+})
+export class HistoryOrderComponent {
+
+}
