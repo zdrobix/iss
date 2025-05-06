967f0379721011de7af1baa591835f98de1a4c6b
zdrobix <zdroba88@gmail.com>
2025-05-06 19:42:30 +0300

    Implemented service for drug storages.

---
+++ visual/PharmacyHospitalUI/src/app/features/inventory/services/storage.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DrugStorage } from '../../models/drug-storage.model';
+import { environment } from 'src/environment/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor(private http: HttpClient) { }
+
+  getStorageById(id: number) : Observable<DrugStorage> {
+    return this.http.get<DrugStorage>(`${environment.apiBaseUrl}/api/drugstorage/${id}`);
+  }
+}
