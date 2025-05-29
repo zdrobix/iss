408241e784e8e0ad046d8d5f17911117cbb2476a
zdrobix <zdroba88@gmail.com>
2025-05-29 18:27:05 +0300

    Swagger usage.

--- api/PharmacyApi/PharmacyApi/Program.cs
+++ api/PharmacyApi/PharmacyApi/Program.cs
@@ -43,11 +43,15 @@ builder.WebHost.UseUrls($"http://*:{port}");
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//	app.UseSwagger();
+//	app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
